9c06cb308f54f8f9b93131fc8f819f8c34171988
ManelMora <dragonesalpoder@gmail.com>
2024-02-06 16:01:54 +0100

    Cambios2

--- servicios/core/urls.py
+++ servicios/core/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('api/ListAll_Customer', views.CustomerAllViewSet.as_view()), #http://127.0.0.1:8000/api/ListAll_Customer
-    path('Create_Customer', views.CustomerCreate.as_view()), #http://127.0.0.1:8000/api/Create_Customer
-    path('Delete_Customer/<customer_id>', views.CustomerDelete.as_view()), #http://127.0.0.1:8000/api/Delete_Customer
-    path('Update_Customer/<customer_id>', views.CustomerUpdate.as_view()), #http://127.0.0.1:8000/api/Update_Customer
+    path('api/Create_Customer', views.CustomerCreate.as_view()), #http://127.0.0.1:8000/api/Create_Customer
+    path('api/Delete_Customer/<customer_id>', views.CustomerDelete.as_view()), #http://127.0.0.1:8000/api/Delete_Customer
+    path('api/Update_Customer/<customer_id>', views.CustomerUpdate.as_view()), #http://127.0.0.1:8000/api/Update_Customer
 ]
+
+#Nota el delete_Customer <> hay que borrarlo y pones el id
--- servicios/servicios/settings.py
+++ servicios/servicios/settings.py
@@ -47,7 +47,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     ),
 }
 MIDDLEWARE = [
